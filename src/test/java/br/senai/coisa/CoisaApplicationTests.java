b49d8f19995dd87c407779bac1386829df8a6174
profcarlos-senai <carlos.correia@sistemafiep.org.br>
2025-03-13 15:53:10 -0300

    inicial

---
+++ src/test/java/br/senai/coisa/CoisaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senai.coisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
